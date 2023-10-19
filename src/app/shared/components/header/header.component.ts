b483aaa374b3a7d9f89800f849ed31f5ffcdf449
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-10-19 13:05:34 -0600

    Se crea header

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'shared-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  @Output() toggle = new EventEmitter<boolean>();
+  menuStatus: boolean = false;
+
+  cerrar() {
+    this.menuStatus = !this.menuStatus;
+    this.toggle.emit(this.menuStatus);
+
+    console.log(this.menuStatus);
+  }
+}
