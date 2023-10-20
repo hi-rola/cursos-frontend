e32c6145e2340ed536d7c51c5df7453ebd242f03
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-10-20 16:15:35 -0600

    Se agrega servicio para manejar peticiones hacia el backend

--- src/app/usuarios/services/usuarios.service.ts
+++ src/app/usuarios/services/usuarios.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { response } from 'express';
+import { Observable, map } from 'rxjs';
 import { Usuario } from 'src/app/auth/interfaces/AuthResponse';
 import { enviroments } from 'src/enviroments/enviroments';
 
@@ -14,5 +15,36 @@ export class UsuariosService {
 
   getUsuarios(): Observable<Usuario[]> {
     return this.http.get<Usuario[]>(`${this.baseUrl}/usuarios-estudiante`);
+  }
+
+  getUsuarioById(id_usuario: number): Observable<Usuario> {
+    return this.http.get<Usuario>(
+      `${this.baseUrl}/usuarios-estudiante/` + id_usuario
+    );
+  }
+
+  updateInformacionUsuario(
+    id_usuario: number,
+    usuario: Usuario
+  ): Observable<Usuario | string> {
+    return this.http
+      .put<Usuario>(
+        `${this.baseUrl}/usuarios-estudiante/` + id_usuario,
+        usuario
+      )
+      .pipe(map((response) => response.mensaje!));
+  }
+
+  updateEstatusUsuario(id_usuario: number, estado: number): Observable<string> {
+    const body = { estado };
+    return this.http
+      .patch<Usuario>(`${this.baseUrl}/usuarios-estudiante/` + id_usuario, body)
+      .pipe(map((response) => response.mensaje!));
+  }
+
+  registrar(usuario: Usuario): Observable<Usuario | string> {
+    return this.http
+      .post<Usuario>(`${this.baseUrl}/auth/registrar`, usuario)
+      .pipe(map((response) => response.mensaje!));
   }
 }
