50859b13686920afeafd116f3c9de09eee187d95
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-11-06 17:37:46 -0600

    Se diseña la interfaz principal de estadisticas

---
+++ src/app/dashboard/pages/estadisticas/estadisticas.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard-estadisticas',
+  templateUrl: './estadisticas.component.html',
+  styleUrls: ['./estadisticas.component.css'],
+})
+export class EstadisticasComponent {}
