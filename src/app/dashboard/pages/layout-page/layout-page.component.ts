6a825686587da7c4d6262f81a88039d317c5aa22
Jose Rolando Garcia <rolaalbaa@gmail.com>
2023-11-06 17:46:23 -0600

    Se agrega main page

---
+++ src/app/dashboard/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard-layout-page',
+  templateUrl: './layout-page.component.html',
+  styleUrls: ['./layout-page.component.css'],
+})
+export class LayoutPageComponent {}
